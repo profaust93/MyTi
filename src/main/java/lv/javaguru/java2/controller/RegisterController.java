a5726d891208564d849de2a1f052983b14dfc5e2
KleanupGuy <kemran15@mail.ru>
2016-04-04 21:50:13 +0300

    Merge branch 'master' of E:\IdeaProjects\MyTi2 with conflicts.

---
+++ src/main/java/lv/javaguru/java2/controller/RegisterController.java
@@ -0,0 +1,59 @@
+package lv.javaguru.java2.controller;
+
+import lv.javaguru.java2.database.DBException;
+import lv.javaguru.java2.database.UserDAO;
+import lv.javaguru.java2.database.jdbc.UserDAOImpl;
+import lv.javaguru.java2.domain.User;
+import lv.javaguru.java2.model.MVCModel;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+/**
+ * Created by Kemran on 02/04/2016.
+ */
+public class RegisterController implements MVCController {
+
+
+    @Override
+    public MVCModel processGet(HttpServletRequest req) {
+        return new MVCModel("/register.jsp", "Registration");
+    }
+
+    @Override
+    public MVCModel processPost(HttpServletRequest req) {
+        User user = new User(
+                req.getParameter("login"),
+                req.getParameter("password"),
+                req.getParameter("firstname"),
+                req.getParameter("lastname"),
+                req.getParameter("email")
+        );
+        HttpSession ses = req.getSession();
+        UserDAO userDAO = new UserDAOImpl();
+        User checkUser = null;
+
+        try {
+            checkUser = userDAO.getUserByEmailOrLogin(user.getLogin());
+
+        } catch (DBException e) {
+            return new MVCModel("/register", e.getMessage());
+        }
+
+            if (checkUser == null) {
+                try {
+                    userDAO.create(user);
+                    req.getSession().setAttribute("userId", user.getUserId());
+                    req.getSession().setAttribute("isLoggedIn",true);
+                } catch (DBException e) {
+                    return new MVCModel("/register.jsp", "Error with DB.");
+                }
+            } else {
+                return new MVCModel("/register.jsp", "This login is already taken.");
+            }
+
+        return new MVCModel("/register.jsp", null);
+    }
+}
--- src/main/java/lv/javaguru/java2/model/user/UserModelImpl.java
+++ src/main/java/lv/javaguru/java2/model/user/UserModelImpl.java
@@ -1,11 +1,13 @@
 package lv.javaguru.java2.model.user;
 
-import javax.servlet.http.HttpServletRequest;
+import com.sun.deploy.net.HttpRequest;
 import lv.javaguru.java2.database.DBException;
 import lv.javaguru.java2.database.UserDAO;
 import lv.javaguru.java2.domain.User;
 import lv.javaguru.java2.model.exceptions.LoginException;
+import lv.javaguru.java2.model.exceptions.RegisterException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -38,17 +40,50 @@ public class UserModelImpl implements UserModel {
     }
 
     @Override
-    public Boolean registerUser(HttpServletRequest request) {
+    public Boolean registerUser(HttpServletRequest req) throws RegisterException {
         // Добавить  парметры с формы в объект User пример user.setName(request.getParameters("name"))
+        User user = null;
+        try {
+            user.setLogin(req.getParameter("login"));
+            user.setPassword(req.getParameter("password"));
+            user.setFirstName(req.getParameter("firstname"));
+            user.setLastName(req.getParameter("lastname"));
+            user.setEmail(req.getParameter("email"));
+        } catch (NullPointerException e){
+            throw new RegisterException(e.getMessage());
+        }
         // также проверять обязательный праметры типо логина пароля и т.д чтобы не были пустыми
+        public boolean checkFields(User user){
+            return !(user.getLogin().isEmpty()
+                    || user.getPassword().isEmpty()
+                    || user.getEmail().isEmpty()
+                    || user.getFirstName().isEmpty()
+                    || user.getLastName().isEmpty());
+        }
         // если что-то плохо выкинуть эксепшн типо throw new RegistrationException("Login is empty")
+        if(!checkFields(user)){
+            throw new RegisterException("Login is empty");
         //  если все ок вернуть true на всякий пожарный
+        }else{
+            return true;
+        }
         // чтобы вывести пользователя вызови userDAO.create(user)
+        try {
+            userDAO.create(user);
+            userDAO.getUserByEmailOrLogin(user.getLogin());
+         //......
         // обзятельно лови эксепшены смотри метод выше
-        return null;
+        } catch (DBException e){
+            throw new RegisterException(e.getMessage());
+        }
+
     }
+
+
 
     public void setUserDAO(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
+
+
 }
