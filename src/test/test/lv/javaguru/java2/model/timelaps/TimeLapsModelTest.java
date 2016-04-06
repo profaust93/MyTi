8817653e6072f85ac20c9b9fa713bb9f945bfb78
ruslan <profaust93@gmail.com>
2016-04-06 19:07:30 +0300

    Model test with simple (stream too)

--- src/test/test/lv/javaguru/java2/model/timelaps/TimeLapsModelTest.java
+++ src/test/test/lv/javaguru/java2/model/timelaps/TimeLapsModelTest.java
@@ -43,6 +43,18 @@ public class TimeLapsModelTest {
 
         timeLapsModel.addTimeLaps(timeLaps);
 
+
+        List<TimeLapsList> timeLapsList = timeLapsModel.getAllTimeLapsForUser("1");
+
+        Predicate<TimeLapsList> timeLapsId = timeLaps1 -> Long.valueOf(666) ==
+                timeLaps1.getTimeLapsId();
+
+        Optional<TimeLapsList> myTimeLapsId = timeLapsList.stream()
+                .filter(timeLapsId).findAny();
+
+        assertFalse(myTimeLapsId.isPresent());
+
+
         Boolean status = true;
         for(Map.Entry entry:timeLapsModel.addTimeLaps(timeLaps).entrySet()) {
             String value = (String) entry.getValue();
